82ab593ed4677c95d53bfc42af489aababc51587
Cellie <CelliesProjects@users.noreply.github.com>
2025-03-17 21:52:45 +0100

    Fixes and cleanup

--- src/OpenStreetMap-esp32.cpp
+++ src/OpenStreetMap-esp32.cpp
@@ -36,7 +36,7 @@ void OpenStreetMap::PNGDraw(PNGDRAW *pDraw)
     currentInstance->png.getLineAsRGB565(pDraw, destRow, PNG_RGB565_BIG_ENDIAN, 0x0000);
 }
 
-void OpenStreetMap::computeRequiredTiles(double longitude, double latitude, uint8_t zoom, tileVector &requiredTiles)
+void OpenStreetMap::computeRequiredTiles(double longitude, double latitude, uint8_t zoom, tileList &requiredTiles)
 {
     // Compute exact tile coordinates
     const double exactTileX = lon2tile(longitude, zoom);
@@ -100,13 +100,13 @@ void OpenStreetMap::computeRequiredTiles(double longitude, double latitude, uint
 bool OpenStreetMap::isTileCached(uint32_t x, uint32_t y, uint8_t z)
 {
     for (const auto &tile : tilesCache)
-        if (tile.valid && tile.x == x && tile.y == y && tile.z == z)
+        if (tile.x == x && tile.y == y && tile.z == z && tile.valid)
             return true;
 
     return false;
 }
 
-CachedTile *OpenStreetMap::findUnusedTile(const tileVector &requiredTiles, uint8_t zoom)
+CachedTile *OpenStreetMap::findUnusedTile(const tileList &requiredTiles, uint8_t zoom)
 {
     for (auto &tile : tilesCache)
     {
@@ -114,7 +114,7 @@ CachedTile *OpenStreetMap::findUnusedTile(const tileVector &requiredTiles, uint8
         bool needed = false;
         for (const auto &[x, y] : requiredTiles)
         {
-            if (tile.valid && tile.x == x && tile.y == y && tile.z == zoom)
+            if (tile.x == x && tile.y == y && tile.z == zoom && tile.valid)
             {
                 needed = true;
                 break;
@@ -161,7 +161,7 @@ bool OpenStreetMap::resizeTilesCache(uint8_t cacheSize)
     return true;
 }
 
-void OpenStreetMap::updateCache(tileVector &requiredTiles, uint8_t zoom)
+void OpenStreetMap::updateCache(tileList &requiredTiles, uint8_t zoom)
 {
     for (const auto &[x, y] : requiredTiles)
     {
@@ -175,7 +175,7 @@ void OpenStreetMap::updateCache(tileVector &requiredTiles, uint8_t zoom)
 
             log_i("%s", result.c_str());
         }
-    }    
+    }
 }
 
 bool OpenStreetMap::fetchMap(LGFX_Sprite &mapSprite, double longitude, double latitude, uint8_t zoom)
@@ -202,7 +202,7 @@ bool OpenStreetMap::fetchMap(LGFX_Sprite &mapSprite, double longitude, double la
     longitude = fmod(longitude + 180.0, 360.0) - 180.0;
     latitude = std::clamp(latitude, -90.0, 90.0);
 
-    tileVector requiredTiles;
+    tileList requiredTiles;
     computeRequiredTiles(longitude, latitude, zoom, requiredTiles);
 
     if (tilesCache.capacity() < requiredTiles.size())
@@ -211,19 +211,7 @@ bool OpenStreetMap::fetchMap(LGFX_Sprite &mapSprite, double longitude, double la
         return false;
     }
 
-    for (const auto &[x, y] : requiredTiles)
-    {
-        if (!isTileCached(x, y, zoom))
-        {
-            CachedTile *tileToReplace = findUnusedTile(requiredTiles, zoom);
-            String result;
-            const bool success = downloadAndDecodeTile(*tileToReplace, x, y, zoom, result);
-            if (!success)
-                log_e("%s", result.c_str());
-
-            log_i("%s", result.c_str());
-        }
-    }
+    updateCache(requiredTiles, zoom);
 
     if (mapSprite.width() != mapWidth || mapSprite.height() != mapHeight)
     {
@@ -409,22 +397,22 @@ bool OpenStreetMap::saveMap(const char *filename, LGFX_Sprite &sprite, String &r
     uint32_t biClrImportant = 0;
 
     // Write BMP header
-    file.write(reinterpret_cast<const uint8_t*>(&bfType), sizeof(bfType));
-    file.write(reinterpret_cast<const uint8_t*>(&bfSize), sizeof(bfSize));
-    file.write(reinterpret_cast<const uint8_t*>(&bfReserved), sizeof(bfReserved));
-    file.write(reinterpret_cast<const uint8_t*>(&bfOffBits), sizeof(bfOffBits));
+    file.write(reinterpret_cast<const uint8_t *>(&bfType), sizeof(bfType));
+    file.write(reinterpret_cast<const uint8_t *>(&bfSize), sizeof(bfSize));
+    file.write(reinterpret_cast<const uint8_t *>(&bfReserved), sizeof(bfReserved));
+    file.write(reinterpret_cast<const uint8_t *>(&bfOffBits), sizeof(bfOffBits));
 
-    file.write(reinterpret_cast<const uint8_t*>(&biSize), sizeof(biSize));
-    file.write(reinterpret_cast<const uint8_t*>(&biWidth), sizeof(biWidth));
-    file.write(reinterpret_cast<const uint8_t*>(&biHeight), sizeof(biHeight));
-    file.write(reinterpret_cast<const uint8_t*>(&biPlanes), sizeof(biPlanes));
-    file.write(reinterpret_cast<const uint8_t*>(&biBitCount), sizeof(biBitCount));
-    file.write(reinterpret_cast<const uint8_t*>(&biCompression), sizeof(biCompression));
-    file.write(reinterpret_cast<const uint8_t*>(&biSizeImage), sizeof(biSizeImage));
-    file.write(reinterpret_cast<const uint8_t*>(&biXPelsPerMeter), sizeof(biXPelsPerMeter));
-    file.write(reinterpret_cast<const uint8_t*>(&biYPelsPerMeter), sizeof(biYPelsPerMeter));
-    file.write(reinterpret_cast<const uint8_t*>(&biClrUsed), sizeof(biClrUsed));
-    file.write(reinterpret_cast<const uint8_t*>(&biClrImportant), sizeof(biClrImportant));
+    file.write(reinterpret_cast<const uint8_t *>(&biSize), sizeof(biSize));
+    file.write(reinterpret_cast<const uint8_t *>(&biWidth), sizeof(biWidth));
+    file.write(reinterpret_cast<const uint8_t *>(&biHeight), sizeof(biHeight));
+    file.write(reinterpret_cast<const uint8_t *>(&biPlanes), sizeof(biPlanes));
+    file.write(reinterpret_cast<const uint8_t *>(&biBitCount), sizeof(biBitCount));
+    file.write(reinterpret_cast<const uint8_t *>(&biCompression), sizeof(biCompression));
+    file.write(reinterpret_cast<const uint8_t *>(&biSizeImage), sizeof(biSizeImage));
+    file.write(reinterpret_cast<const uint8_t *>(&biXPelsPerMeter), sizeof(biXPelsPerMeter));
+    file.write(reinterpret_cast<const uint8_t *>(&biYPelsPerMeter), sizeof(biYPelsPerMeter));
+    file.write(reinterpret_cast<const uint8_t *>(&biClrUsed), sizeof(biClrUsed));
+    file.write(reinterpret_cast<const uint8_t *>(&biClrImportant), sizeof(biClrImportant));
 
     for (int y = 0; y < sprite.height(); y++)
     {
